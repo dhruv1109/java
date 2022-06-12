20f3ac73f2e5c728cead718afe36ceffe7016ce4
dhruv1109 <dhruv11.bansal@gmail.com>
2022-06-12 12:53:52 +0530

    input from user

---
+++ learn/src/io.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class io {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		System.out.print("Enter 2 no : ");
+		Scanner s = new Scanner(System.in);
+		a=s.nextInt();
+		b=s.nextInt();
+		
+		c=a+b;
+		System.out.println("sum of 2 no is : "+ c);
+		
+	}
+
+}
